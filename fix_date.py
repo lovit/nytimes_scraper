a5bcd8f418ee223126365743524488f06af5b050
lovit <soy.lovit@gmail.com>
2019-02-26 03:29:36 +0900

    Fix

--- fix_date.py
+++ fix_date.py
@@ -8,7 +8,7 @@ for path in paths:
     with open(path, encoding='utf-8') as f:
         json_obj = json.load(f)
     date = path.split('/')[-1]
-    date = '{}-{}-{}'.format(date[:4], date[4:6], date[6:])
+    date = '{}-{}-{}'.format(date[:4], date[4:6], date[6:8])
     json_obj['date'] = date
     with open(path, 'w', encoding='utf-8') as f:
         json.dump(json_obj, f, ensure_ascii=False, indent=2)
